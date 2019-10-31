807c53a71b96eaf44d82e87395391d662948e323
lijichao <1159165064@qq.com>
2019-10-31 18:02:34 +0800

    register

--- my-server/routes/register.js
+++ my-server/routes/register.js
@@ -6,15 +6,15 @@ const {
 } = require('../CURD/curd')
 
 router.get("/", async function (req, res, next) {
-    let phone = req.query.phone;
+    let user = req.query.phone;
     let password = req.query.password;;
     let data = await find('user', {
-        phone: 13113419964
+        username: user
     });
     let str = "注册失败，此手机号码已存在"
-    if (phone != data[0].phone) {
+    if (data.length == 0) {
         insert('user', [{
-            phone: phone,
+            username: user,
             password: password
         }])
         str = "注册成功"
